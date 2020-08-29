74ac8722570aa5acd6b14055c7a83d0d2ea65c86
sreerag <sreeragraghunandan@gmail.com>
2020-08-29 22:30:52 +0530

    week3

--- Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,19 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    coin = [10, 5, 1]
+    i = 0
+    n = 0
+    while m != 0:
+        if m < coin[i]:
+            i += 1
+        while m >= coin[i]:
+            m -= coin[i]
+            n += 1 
+    return n
 
-if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+# if __name__ == '__main__':
+#     m = int(sys.stdin.read())
+#     print(get_change(m))
+m = int(input())
+print(get_change(m))
