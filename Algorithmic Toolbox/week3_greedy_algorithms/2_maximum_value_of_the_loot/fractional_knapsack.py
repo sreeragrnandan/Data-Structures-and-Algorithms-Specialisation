37afb35cc09176956589e357250302c2ec2c866d
sreerag <sreeragraghunandan@gmail.com>
2020-08-29 23:57:31 +0530

    week3

--- Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -8,10 +8,37 @@ def get_optimal_value(capacity, weights, values):
     return value
 
 
-if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+# if __name__ == "__main__":
+#     data = list(map(int, sys.stdin.read().split()))
+#     n, capacity = data[0:2]
+#     values = data[2:(2 * n + 2):2]
+#     weights = data[3:(2 * n + 2):2]
+#     opt_value = get_optimal_value(capacity, weights, values)
+#     print("{:.10f}".format(opt_value))
+data = list(map(int, input().split()))
+l = []
+n = data[0]
+W = data[1]
+for i in range(n):
+    t = list(map(int, input().split()))
+    l.append([t[0]/t[1], t[1]])
+l.sort(reverse=True)
+
+TotVal = 0
+i = 0
+while W > 0:
+
+    if l[i][1] == 0:
+        i += 1
+    if i == n:
+        break
+    if W >= l[i][1]:
+        TotVal += l[i][0] * l[i][1]
+        W -= l[i][1]
+        l[i][1] = 0
+    elif W < l[i][1]:
+        TotVal += (l[i][0] * W)
+        l[i][1] -= W
+        W = 0
+
+print("{:.10f}".format(TotVal))
