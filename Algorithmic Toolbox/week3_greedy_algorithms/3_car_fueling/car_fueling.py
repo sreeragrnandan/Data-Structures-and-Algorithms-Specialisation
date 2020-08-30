4e7ade457f7eee82fea669af83a30225b4f70c72
sreerag <sreeragraghunandan@gmail.com>
2020-08-30 13:42:01 +0530

    Week3 Car Fueling

--- Algorithmic Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -4,8 +4,36 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code here
-    return -1
+    # if len(stops) == 0 and distance <= tank:
+    #     return 0
+    # if len(stops) == 0 and distance > tank:
+    #     return -1
 
-if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
-    print(compute_min_refills(d, m, stops))
+    NumTanks = 0
+    i = 0
+    stops = [0] + stops
+    stops.append(distance)
+    n = len(stops)
+
+    currentStop = 0
+    while currentStop < n-1:
+        lastStop = currentStop
+        while currentStop < n-1 and stops[currentStop + 1] - stops[lastStop] <= tank:
+            currentStop += 1
+
+        if currentStop == lastStop and currentStop != n-1:
+            return -1
+        if currentStop < n-1:
+            NumTanks += 1
+
+    return NumTanks
+
+# if __name__ == '__main__':
+#     d, m, _, *stops = map(int, sys.stdin.read().split())
+#     print(compute_min_refills(d, m, stops))
+
+d = int(input())
+m = int(input())
+n = int(input())
+stops = list(map(int, input().split()))
+print(compute_min_refills(d, m, stops))
