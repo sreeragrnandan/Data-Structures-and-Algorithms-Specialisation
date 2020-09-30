908dc91dd7754aa612543b223f0527f9a4f691dc
sreerag <sreeragraghunandan@gmail.com>
2020-09-30 23:46:38 +0530

    lc2,edit

--- Algorithmic Toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ Algorithmic Toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,24 @@
 # Uses python3
 def edit_distance(s, t):
     #write your code here
-    return 0
+    m = len(s)
+    n = len(t)
+    d = [[0 for j in range(m+2)] for i in range(n+2)]
+    for i in range(m+1):
+        for j in range(n+1):
+            if(i==0):
+                d[i][j] = j
+            elif j == 0:
+                d[i][j] = i
+            elif s[i-1] == t[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+               d[i][j] = min(d[i][j-1], d[i-1][j], d[i-1][j-1]) + 1
+    
+    return d[m][n]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
+# s = input()
+# t = input()
+# print(edit_distance(s,t))
--- Algorithmic Toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ Algorithmic Toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -2,21 +2,35 @@
 
 import sys
 
-def lcs2(a, b):
+def lcs2(a, b,m,n):
     #write your code here
-    return min(len(a), len(b))
+    d = [[None]*(n + 1) for i in range(m + 1)]
+    for i in range(0,m+1):
+        for j in range(0, n+1):
+            if i == 0 or j == 0:
+                d[i][j] = 0
+            elif a[i-1] == b[j-1]:
+                d[i][j] = d[i-1][j-1] + 1
+            else:
+                d[i][j] = max(d[i-1][j], d[i][j-1])
+    return d[m][n]
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     data = list(map(int, input.split()))
 
-    n = data[0]
-    data = data[1:]
-    a = data[:n]
+#     n = data[0]
+#     data = data[1:]
+#     a = data[:n]
 
-    data = data[n:]
-    m = data[0]
-    data = data[1:]
-    b = data[:m]
+#     data = data[n:]
+#     m = data[0]
+#     data = data[1:]
+#     b = data[:m]
 
-    print(lcs2(a, b))
+#     print(lcs2(a, b))
+m = int(input())
+a = list(map(int,input().split()))
+n = int(input())
+b = list(map(int,input().split()))
+print(lcs2(a,b,m,n))
